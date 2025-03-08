0bb310f8c4ab8e1c1a4ac7ec2d6078bf39bc81c3
Konstantin Berg <163849606+it2konst@users.noreply.github.com>
2025-03-08 20:08:38 +0700

    feat: section best added

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,33 @@
-// Галерея и лайтбоксы от Fancybox
-import { Fancybox } from '@fancyapps/ui';
-import '@fancyapps/ui/dist/fancybox/fancybox.css';
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
 
-Fancybox.bind('[data-fancybox]', {
-	// Your custom options
+// import styles bundle
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+
+	slidesPerView: 2,
+	spaceBetween: 16,
+	// loop: true,
+
+	breakpoints: {
+		780: {
+			slidesPerView: 3,
+			spaceBetween: 24,
+		}
+	},
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
 });
 
 // Мобильная навигация
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+
+
+// Аккордеон для мобильной навигации
+import accordion from './modules/accordion.js';
+accordion();
